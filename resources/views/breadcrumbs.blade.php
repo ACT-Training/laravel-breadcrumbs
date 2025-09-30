d06645e06ad4ffa4aab57ddb399c3e47415e50f5
Simon Barrett <simonbarrett@acttraining.org.uk>
2025-09-30 14:40:54 +0100

    :bug: fixed issue with icon text display

--- resources/views/breadcrumbs.blade.php
+++ resources/views/breadcrumbs.blade.php
@@ -1,12 +1,26 @@
+@php
+    $homeRoute = config('breadcrumbs.home_route', 'dashboard');
+    $homeDisplay = config('breadcrumbs.home_display', 'icon');
+    $homeIcon = config('breadcrumbs.home_icon', 'house');
+@endphp
+
 @if (count($breadcrumbs) > 0)
     <div {{ $attributes->merge(['class' => 'mt-1 flex items-center']) }}>
         <flux:breadcrumbs class="text-sm font-medium sm:text-lg">
             @foreach ($breadcrumbs as $breadcrumb)
+                @php
+                    $isHome = $breadcrumb->title === ucfirst($homeRoute) && $loop->first;
+                @endphp
+
                 @if ($breadcrumb->url && !$loop->last)
-                    @if ($breadcrumb->title === 'Dashboard' && $loop->first)
+                    @if ($isHome)
                         <flux:breadcrumbs.item href="{{ $breadcrumb->url }}">
-                            <flux:icon icon="house" class="size-5 !text-orange-500 hover:!text-orange-600" />
-                            {{ $breadcrumb->title }}
+                            @if (in_array($homeDisplay, ['icon', 'both']))
+                                <flux:icon icon="{{ $homeIcon }}" class="size-5 !text-orange-500 hover:!text-orange-600" />
+                            @endif
+                            @if (in_array($homeDisplay, ['text', 'both']))
+                                {{ $breadcrumb->title }}
+                            @endif
                         </flux:breadcrumbs.item>
                     @else
                         <flux:breadcrumbs.item
@@ -17,10 +31,14 @@
                         </flux:breadcrumbs.item>
                     @endif
                 @else
-                    @if ($breadcrumb->title === 'Dashboard' && $loop->first)
+                    @if ($isHome)
                         <flux:breadcrumbs.item class="!text-orange-500">
-                            <flux:icon icon="house" class="size-5" />
-                            {{ $breadcrumb->title }}
+                            @if (in_array($homeDisplay, ['icon', 'both']))
+                                <flux:icon icon="{{ $homeIcon }}" class="size-5" />
+                            @endif
+                            @if (in_array($homeDisplay, ['text', 'both']))
+                                {{ $breadcrumb->title }}
+                            @endif
                         </flux:breadcrumbs.item>
                     @else
                         <flux:breadcrumbs.item class="!text-sm !text-zinc-400 sm:!text-lg dark:!text-zinc-500">
